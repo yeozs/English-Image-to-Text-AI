c6b27b2a49a99ce96bcd44835142d3c1807424b7
Yeo-Zuosheng <yeo.fmy@gmail.com>
2021-12-21 21:52:36 +0800

    Add files via upload
    Initial Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Why do this: Can be used to convert pdf, whatsapp/telegram screenshots into text. AI converter.
+
+#Line 10: This part above will have error. Need to install tesseract everytime you open this document. Delete line 10-12. Then reinput line 10-12
+
+from PIL import Image
+import os
+import pytesseract
+
+
+
+path = "/home/runner/.apt/usr/share/tesseract-ocr/4.00/tessdata/osd.traineddata"
+if os.path.exists(path):
+  print("Already installed!")
+else:
+  os.system("install-pkg tesseract-ocr") #install tesseract locally
+
+#This part above will have error. Need to install tesseract
+
+pytesseract.pytesseract.tesseract_cmd = "tesseract" #put tesseract executable in your path
+
+os.environ["TESSDATA_PREFIX"] = "/home/runner/.apt/usr/share/tesseract-ocr/4.00/tessdata/" #go to environment of tesseract called TESSDATA_PREFIX. Get tessdata as environment
+
+# List of available languages
+print(pytesseract.get_languages(config=''))
+
+text = pytesseract.image_to_string(Image.open('test4.png'))
+
+g = open('test.docx','w+') #wipe previous csv file data
+g.close()
+
+with open('test.docx', mode = 'w') as f:
+    f.write(text)
+    
+print(text)
+
+
+#https://tesseract-ocr.github.io/tessdoc/Data-Files.html => other language trained data
+
+# French text image to string
+#print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
+
+#in case of corruption: https://replit.com/talk/ask/Pytesseract-Auto-Shell-Install-Package-Issue/140049
+
